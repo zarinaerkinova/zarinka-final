51d7f09ec0bf5d36ecd38634bc9cd84014ab44f0
Zarina <107386522+zarinaerkinova@users.noreply.github.com>
2025-09-23 12:42:41 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,9 +6,9 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://zarinka-final.onrender.com',
         changeOrigin: true,
-        secure: false,
+        secure: true,
       },
     },
   },
